18711352c8f019ef9dc045cd804b933f2a811900
Karen Lopez <k.alicia.lopez@gmail.com>
2023-11-30 00:56:30 +0800

    Forms set up

---
+++ she_codes_news/news/forms.py
@@ -0,0 +1,19 @@
+#news/forms/py
+from django import forms
+from django.forms import ModelForm
+from .models import NewsStory
+
+class StoryForm(ModelForm): # new StoryForm class is inheriting from the ModelForm class imported from django.forms
+    class Meta:
+        model = NewsStory # we're telling the form that it's going to be used to create a NewsStory object
+        fields = ['title', 'author', 'pub_date', 'content'] # we're telling the form which fields to include
+        widgets = {
+            'pub_date': forms.DateInput(
+                format='%m/%d/%Y',
+                attrs={
+                    'class':'form-control',
+                    'placeholder':'Select a date',
+                    'type':'date'
+                }
+            ),
+        }
--- she_codes_news/news/views.py
+++ she_codes_news/news/views.py
@@ -1,5 +1,7 @@
 from django.views import generic
+from django.urls import reverse_lazy
 from .models import NewsStory
+from .forms import StoryForm
 
 
 class IndexView(generic.ListView):
@@ -8,11 +10,11 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         '''Return all news stories.'''
-        return NewsStory.objects.all()
+        return NewsStory.objects.all().order_by('-pub_date')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['latest_stories'] = NewsStory.objects.all()[:4]
+        context['latest_stories'] = NewsStory.objects.all().order_by('-pub_date')[:4]
         return context
 
 class StoryView(generic.DetailView):
@@ -20,7 +22,8 @@ class StoryView(generic.DetailView):
     template_name = 'news/story.html'
     context_object_name = "story"
 
-    # def get_context_data(self, **kwargs):
-    #     context = super().get_context_data(**kwargs)
-    #     context['latest_stories'] = NewsStory.objects.all()[:4]
-    #     return context
+class AddStoryView(generic.CreateView): # CreateView is a generic view that allows us to create a new object. Our subclass AddStoryView inherits from this base view.
+    form_class = StoryForm # what form is used for the AddStoryView
+    context_object_name = 'storyform' # what variable name should be used to store that form under in the template
+    template_name = 'news/createStory.html' # which template the view should use
+    success_url = reverse_lazy('news:index') # where the user should be redirected to after successfully submitting the form
